5f1b3c82283c18b82523de7fc29e2f92cf176f19
ErsyaHasby <hasbysatria54@gmail.com>
2024-12-30 10:29:57 +0700

    Header ADT account
    Mendefinisikan struktur data untuk akun pengguna dan deklarasi fungsi untuk registrasi dan login akun

---
+++ account.h
@@ -0,0 +1,31 @@
+// nama program: account.h
+//  deskripsi: File header ini mendefinisikan struktur data untuk akun pengguna dan deklarasi fungsi untuk registrasi dan login akun.
+//  pembuat: Varian Abidarma Syuhada & Ersya Hasby Satria
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+int registerAccount();
+int loginAccount();
+
+typedef struct
+{
+    char username[50];
+    char password[50];
+} Account;
+
+#endif
+
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+int registerAccount();
+int loginAccount();
+
+typedef struct
+{
+    char username[50];
+    char password[50];
+} Account;
+
+#endif
